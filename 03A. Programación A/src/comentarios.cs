1a18cbde3d211bd79caaf0d96aca85afe32aee9f
José Luis Blanco <joseluisbn@gmail.com>
2020-11-24 17:38:40 +0100

    Create comentarios.cs

---
+++ 03A. Programación A/src/comentarios.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esto es un comentario de una línea.
+
+            Console.WriteLine("Los comentarios son omitidos por el compilador");
+
+            /*Esto es un comentario multilínea.
+             * Estos textos quedan fuera del código
+             * al compilarlo. Su uso es para ayudar
+             * al desarrollador.
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
