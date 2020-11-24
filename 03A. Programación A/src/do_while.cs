cc5226e38c5646ecfe4c0104c4aad804a264b3aa
José Luis Blanco <joseluisbn@gmail.com>
2020-11-24 17:43:09 +0100

    Create do_while.cs

---
+++ 03A. Programación A/src/do_while.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace CSharpLearningProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos la variable
+            int a = 5;
+
+            //Definimos el bucle do-while
+
+            do
+            {
+                Console.WriteLine("Este es un bucle do-while");
+            } while (a == 6);
+
+            /*Este bucle realiza la acción al menos una vez
+             y se detiene cuando la premisa del while se cumple*/
+
+            Console.ReadKey();
+        }
+    }
+}
