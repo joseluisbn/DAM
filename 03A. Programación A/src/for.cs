9e0c9f811a725a9b451f53ce022617d11bf3eeb1
José Luis Blanco <joseluisbn@gmail.com>
2020-11-24 17:42:13 +0100

    Create for.cs

---
+++ 03A. Programación A/src/for.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CSharpLearningProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Definición del bucle
+
+            for (int i = 0; i<15; i++)
+            {
+                Console.WriteLine("Ejemplo de bucle for");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
