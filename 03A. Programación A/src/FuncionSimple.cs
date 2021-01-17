5c299aa078cc15758a3680db99f49a90a378e5cb
José Luis Blanco <joseluisbn@gmail.com>
2021-01-17 11:50:08 +0100

    Create FuncionSimple.cs

---
+++ 03A. Programación A/src/FuncionSimple.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace FuncionSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 54;
+
+            // creamos una variable (aunque no es necesario) para realizar la llamada a la función
+
+            int final = suma(a, b);
+
+            Console.WriteLine(final);
+        }
+
+        //Creación de la función fuera del método Main. Ponemos la directiva static
+        static int suma(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+    }
+}
